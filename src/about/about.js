171549ff4b11f0bd62f8156b08d33ff1e51e65cd
RD1878 <khokhlovpavel1986@yandex.ru>
2020-04-04 12:27:05 +0300

    Верстка О пректе без нужных кнопок и пагинации в слайдере

--- src/about/about.js
+++ src/about/about.js
@@ -1,1 +1,29 @@
-import './index.css';
+import './about.css';
+
+import '../../node_modules/swiper/css/swiper.min.css';
+import Swiper from '../../node_modules/swiper/js/swiper';
+
+const func = () => {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 3.5,
+    direction: 'horizontal',
+    centeredSlides: true,
+    loop: true,
+    updateOnWindowResize: true,
+    watchOverflow: true,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+    //effect: 'slide',
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
+};
+
+export { func };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './index.css';
+
+import { func } from './about/about';
+
+func();
